5209c0e35777714da47825970bec9f65c94948d0
Harold Baur <harold.baur@enexis.nl>
2026-03-05 19:47:23 +0100

    Fixed SDTNamespaceContext

--- src/main/java/be/baur/sdt/xpath/SDTNamespaceContext.java
+++ src/main/java/be/baur/sdt/xpath/SDTNamespaceContext.java
@@ -5,7 +5,6 @@ import static be.baur.sdt.xpath.SDTFunctionContext.FUNCTIONS_NS_URI;
 import static be.baur.sdt.xpath.SDTFunctionContext.W3C_FUNCTIONS_NS_PFX;
 import static be.baur.sdt.xpath.SDTFunctionContext.W3C_FUNCTIONS_NS_URI;
 
-import java.io.Serializable;
 import java.util.Objects;
 
 import org.jaxen.NamespaceContext;
@@ -17,28 +16,26 @@ import org.jaxen.SimpleNamespaceContext;
  * 
  * @see NamespaceContext
  */
-public final class SDTNamespaceContext implements NamespaceContext, Serializable
-{
-	private static final long serialVersionUID = -5838697281768509583L;
+public final class SDTNamespaceContext implements NamespaceContext {
 
-	// this class is a wrapper backed by a simple namespace context
-	private static final SimpleNamespaceContext NC = new SimpleNamespaceContext();
- 
-	// pre-register SDT specific bindings
+	// this class is a wrapper backed by a pre-registered namespace context
+	private static final SimpleNamespaceContext PREREG = new SimpleNamespaceContext();
+
+	// register SDT specific bindings
 	static {
-		NC.addNamespace(FUNCTIONS_NS_PFX, FUNCTIONS_NS_URI);
-		NC.addNamespace(W3C_FUNCTIONS_NS_PFX, W3C_FUNCTIONS_NS_URI);
+		PREREG.addNamespace(FUNCTIONS_NS_PFX, FUNCTIONS_NS_URI);
+		PREREG.addNamespace(W3C_FUNCTIONS_NS_PFX, W3C_FUNCTIONS_NS_URI);
 	}
+
+
+	// this instance namespace context for custom bindings
+	private final SimpleNamespaceContext nscontext;
 	
 	/**
-	 * Create a new namespace context that includes the SDT extension bindings.
+	 * Create a new SDT namespace context.
 	 */
-	public SDTNamespaceContext() {}
-
-
-	@Override
-	public String translateNamespacePrefixToUri(String prefix) {
-		return NC.translateNamespacePrefixToUri(prefix);
+	public SDTNamespaceContext() {
+		 nscontext = new SimpleNamespaceContext();
 	}
 
 
@@ -48,18 +45,29 @@ public final class SDTNamespaceContext implements NamespaceContext, Serializable
 	 * 
 	 * @param prefix a namespace prefix, not null
 	 * @param URI    a namespace URI, not null
-	 * @returns true if the context was changes as a result of this call
+	 * @returns true if the context changed as a result of this call
 	 */
 	public boolean addNamespace(String prefix, String URI) {
 
 		Objects.requireNonNull(prefix, "prefix must not be null");
 		Objects.requireNonNull(URI, "URI must not be null");
 
-		if (NC.translateNamespacePrefixToUri(prefix) == null) {
-			NC.addNamespace(prefix, URI);
-			return true;
+		if (PREREG.translateNamespacePrefixToUri(prefix) == null) {
+
+			if (nscontext.translateNamespacePrefixToUri(prefix) == null) {
+				nscontext.addNamespace(prefix, URI);
+				return true;
+			}
 		}
 		return false;
 	}
 
+
+	@Override
+	public String translateNamespacePrefixToUri(String prefix) {
+		
+		// first check pre-registered context, then this instance
+		String URI = PREREG.translateNamespacePrefixToUri(prefix);
+		return URI == null ? nscontext.translateNamespacePrefixToUri(prefix) : URI;
+	}
 }
